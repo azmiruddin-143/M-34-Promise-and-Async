5f4f51a5983d89905cdff15132795ee2ea04eed5
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-10-02 20:02:43 +0600

    allfatch seystem and promise

---
+++ async.js
@@ -0,0 +1,23 @@
+// node async.js //
+// console.log("console - 1");
+// console.log("console - 2");
+// consolegfdfg()
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(pev => pev.json())
+//     .then(data => console.log(data[0].name))
+// console.log("console - 4");
+// console.log("console - 5");
+// setTimeout(() => {
+//     console.log("console - 6");
+// }, 600)
+// let sum = 0
+// setInterval(() => {
+//     if (sum <= 4) {
+//         console.log(sum++);
+//         clearInterval()
+//     }
+    
+// }, 1500)
+// function consolegfdfg() {
+//     console.log("console - 3")
+// }
---
+++ promise.js
@@ -0,0 +1,38 @@
+// node Promise.js
+
+
+let promisefunction = () => {
+      
+      return new Promise((profoseAccept,proposeReject)=>{
+              
+        let man = 60
+        if(man < 45){
+            profoseAccept("Tumake ami valobasi")
+        }
+        else{
+            proposeReject("Tumake ami valobasi na")
+        }
+       
+     })
+}
+
+ promisefunction()
+ .then(data => console.log(data))
+ .catch(err => console.log("",err))
+
+
+// let loader = () => {
+//     return new Promise((resolve, reject) => {
+//         let marrid = Math.random()
+//         if (marrid <= 0.5) {
+//             resolve(marrid)
+//         }
+//         else {
+//             reject(marrid)
+//         }
+//     })
+// }
+
+// loader()
+//     .then(data => console.log("resolve data", data))
+//     .catch(err => console.log("bul oice",err))
